b382018a4c426881cb4bf557e6b60587c4c3f3ad
Magnus Unger <magnus_unger@hotmail.com>
2015-05-18 23:40:32 +0200

    Made buildscript more project independent

--- build.cmd
+++ build.cmd
@@ -1,16 +1,29 @@
 @echo Off
 
-set solutionName=KustobsarApi.sln
-set MSBuildExe="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe"
+set solutionName=
+for %%i in (*.sln) do set solutionName=%%i
+
+if "%solutionName%" == "" (
+	Echo No Solution file found in folder
+	Exit /b
+)
 
 set config=%1
 if "%config%" == "" (
 	set config=Release
 )
 
-set buildDir=Build\%config%
+for /f "tokens=1-3 delims=-" %%a in ("%DATE%") do (set mydate=%%a%%b%%c)
+for /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
+
+set buildTempDir=Build\Temp
+set buildOutDir=Build\%config%\%mydate%_%mytime%
 
 
 nuget restore
-%MSBuildExe% %solutionName% /t:Rebuild /p:Configuration=%config%;OutDir="%cd%\%buildDir%";UseWPP_CopyWebApplication=True;PipelineDependsOnBuild=False
+msbuild %solutionName% /t:Rebuild /p:Configuration=%config%;OutDir="%cd%\%buildTempDir%";UseWPP_CopyWebApplication=True;PipelineDependsOnBuild=False
 
+set webProjectOutputFolder=
+for /d %%i in (%buildTempDir%\_PublishedWebsites\*) do set webProjectOutputFolder=%%i
+
+robocopy %webProjectOutputFolder% %buildOutDir%\Site /MIR /R:5 /W:1 /FFT
