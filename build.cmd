9297eb64f0e6a7f86f4d87ec5ffee4a838b6a894
Magnus Unger <magnus_unger@hotmail.com>
2015-05-20 19:42:40 +0200

    fixed padding zeros on time in foldername, added zipping of outputfolder

--- build.cmd
+++ build.cmd
@@ -14,7 +14,8 @@ if "%config%" == "" (
 )
 
 for /f "tokens=1-3 delims=-" %%a in ("%DATE%") do (set mydate=%%a%%b%%c)
-for /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
+for /f "tokens=1-2 delims=/: " %%a in ("%TIME%") do (set mytime=0%%a%%b)
+SET mytime=%mytime:~-4%
 
 set buildTempDir=Build\Temp
 set buildOutDir=Build\%config%\%mydate%_%mytime%
@@ -26,4 +27,10 @@ msbuild %solutionName% /t:Rebuild /p:Configuration=%config%;OutDir="%cd%\%buildT
 set webProjectOutputFolder=
 for /d %%i in (%buildTempDir%\_PublishedWebsites\*) do set webProjectOutputFolder=%%i
 
-robocopy %webProjectOutputFolder% %buildOutDir%\Site /MIR /R:5 /W:1 /FFT
+if not "%webProjectOutputFolder%" == "" (
+	robocopy %webProjectOutputFolder% %buildOutDir%\Site /MIR /R:5 /W:1 /FFT
+
+	REM Zip output folder
+	powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('%buildOutDir%\Site', '%buildOutDir%.zip'); }"
+)
+
